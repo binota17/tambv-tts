e0393cd9d445d05768302f8734c1afd62fdebd24
binota17 <vantambui4@gmail.com>
2024-05-17 10:00:57 +0700

    change name

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -10,11 +10,11 @@
         <button type="submit">Logout</button>
     </form>
     @if(auth()->user()->hasRole('full_access'))
-    <a href="{{ route('admin.index') }}">Index</a>
+    <a href="{{ route('admin.index') }}">Test phân quyền</a>
     @endif
 
     @if(auth()->user()->hasRole('limited_access'))
-    <a href="{{ route('admin.index_limited') }}">Index</a>
+    <a href="{{ route('admin.index_limited') }}">Test phân quyền</a>
     @endif
 </body>
 </html>
